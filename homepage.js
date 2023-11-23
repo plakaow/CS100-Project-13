881116b890c7275912de75e5f2cce64b122eda2e
plakaow555 <plakaow555@gmail.com>
2023-11-24 00:33:48 +0700

    PK saved 24/11/66

---
+++ homepage.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById('nav_menu');
+let navBarToggle = document.getElementById('menu');
+
+navBarToggle.addEventListener("click",function(){
+    mainNav.classList.toggle('active');
+});
