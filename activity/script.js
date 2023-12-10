6fc262bd570969f43479f9024a521b8f32094730
PK <nattanan.jan@dome.tu.ac.th>
2023-12-10 18:15:22 +0700

    PK saved 18:15

--- activity/script.js
+++ activity/script.js
@@ -9,9 +9,7 @@ function submitForm(event) {
     for (const [name, value] of formData) {
       console.log(name, value);
     }
-  
-    // Reset the form after submission
-    document.getElementById("myForm").reset();
+    testResults();
   }
   
   // Attach a submit event listener to the form
@@ -31,12 +29,45 @@ function submitForm(event) {
       const endDate = document.getElementById("endDate").value;
       const workTitle = document.getElementById("workTitle").value;
       const activityType = document.getElementById("activityType").value;
+      const AcademicYear = document.getElementById("AcademicYear").value;
+      const Semester = document.getElementById("Semester").value;
 
-      if(activityType == 'Activity'){
-
-        document.getElementById("img_1").innerHTML = "<img src='../img/chertam_test.jpg'>";
-        document.getElementById("img_2").innerHTML = "<img src='../img/chertam_test.jpg'>";
-
+      switch(activityType){
+        case 'Course':
+          document.getElementById("img_3").innerHTML = "<img src='../img/course_1.jpg'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/course_2.jpg'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/course_3.jpg'>";
+          break;
+        case 'Activity':
+          document.getElementById("img_3").innerHTML = "<img src='../img/activity_1.png'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/activity_2.png'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/activity_3.png'>";
+          break;
+        case 'Work':
+          document.getElementById("img_3").innerHTML = "<img src='../img/work_1.png'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/work_2.jpg'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/work_3.jpg'>";
+          break;
+        case 'Research':
+          document.getElementById("img_3").innerHTML = "<img src='../img/research_1.jpg'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/research_2.jpg'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/research_3.jpg'>";
+          break;
+        case 'Sports':
+          document.getElementById("img_3").innerHTML = "<img src='../img/sport_1.jpg'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/sport_2.jpg'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/sport_3.jpg'>";
+          break;
+        case 'Competition':
+          document.getElementById("img_3").innerHTML = "<img src='../img/Competition_1.jpg'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/Competition_2.jpg'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/Competition_3.jpg'>";
+          break;
+        case 'Computer Science':
+          document.getElementById("img_3").innerHTML = "<img src='../img/comsci_1.jpg'>";
+          document.getElementById("img_4").innerHTML = "<img src='../img/comsci_2.jpg'>";
+          document.getElementById("img_5").innerHTML = "<img src='../img/comsci_3.jpg'>";
+          break;
       }
 
       document.getElementById("FnameLname").innerHTML = "<p>" + fullname + "</p>";
@@ -45,6 +76,8 @@ function submitForm(event) {
       document.getElementById("date").innerHTML = "<p>" + startDate + "</p>";
       document.getElementById("dateEnd").innerHTML = "<p>" + endDate + "</p>";
       document.getElementById("workActTitle").innerHTML = "<p>" + workTitle + "</p>";
+      document.getElementById("academicYear").innerHTML = AcademicYear;
+      document.getElementById("semester").innerHTML = Semester;
       document.getElementById("typeWork").innerHTML = "<p>" + activityType + "</p>";
       document.getElementById("sub_text_1").innerHTML = "<p>" + activityType + "</p>";
       document.getElementById("sub_text_2").innerHTML = "<p>" + activityType + "</p>";
