8c138d93b618c1a74ad30ecf97106d3fc11f2f89
PK <nattanan.jan@dome.tu.ac.th>
2023-12-10 22:27:49 +0700

    saved 22:27

--- activity/script.js
+++ activity/script.js
@@ -37,40 +37,54 @@ function submitForm(event) {
           document.getElementById("img_3").innerHTML = "<img src='../img/course_1.jpg'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/course_2.jpg'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/course_3.jpg'>";
+          document.getElementById("text_1").innerHTML = "Course Evaluation ที่จะบอกว่าเก็บคะแนนนจากส่วนไหนบ้าง และทำโปรเจ็คเหล่านั้นในสัปดาห์ที่เท่าหไหร่";
+          document.getElementById("text_2").innerHTML = "Bridge to CS@TU B.Sc. ที่จะบอกว่าจบจากคอร์สเหล่านี้ไปแล้วจะสามารถนำไปต่อเป็นคอร์สไหนได้บ้างจากแต่ละภาค";
           break;
         case 'Activity':
           document.getElementById("img_3").innerHTML = "<img src='../img/activity_1.png'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/activity_2.png'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/activity_3.png'>";
+          document.getElementById("text_1").innerHTML = "กิจกรรมรับเพื่อนใหม่ธรรมศาสตร์ที่ได้มีการชวนคุณ พิธาลิ้มเจริญรัตณ์ มาเป็นแขกรับเชิญสุดพิเศษในงานนี้อีกด้วย";
+          document.getElementById("text_2").innerHTML = "และในกิจกรรมรับเพื่อนใหม่นี้ยังมีการแสดงโชว์สุดพิเศษมากมายจากชมรมต่างๆภายในมหาวิทยาลัย";
           break;
         case 'Work':
           document.getElementById("img_3").innerHTML = "<img src='../img/work_1.png'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/work_2.jpg'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/work_3.jpg'>";
+          document.getElementById("text_1").innerHTML = "งานสรุปความเข้าใจเกี่ยวกับ static web ที่ได้เรียนไปในครึ่งเทอมแรกและทำออกมาเป็น presentation";
+          document.getElementById("text_2").innerHTML = "เป็นงานที่ทำให้ได้ทบทวนความรู้ของตัวเองในเรื่องการทำ static web ก่อนที่จะขึ้นเรื่อง dynamic web ที่เป็นเรื่องที่ใช้ความชำนาญมากกว่า";
           break;
         case 'Research':
           document.getElementById("img_3").innerHTML = "<img src='../img/research_1.jpg'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/research_2.jpg'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/research_3.jpg'>";
+          document.getElementById("text_1").innerHTML = "Patiphan Phengpao เป็นแชเเนลบนยูทูปที่ดีมากๆสำหรับการหาความรู้นอกห้องเรียนเกี่ยวกับการทำเว็บไซต์ เพราะภายในแชเเนลนี้จะมีการสอนเทคนิคการทำเว็บที่เข้าใจง่ายเเละใช้งานได้จริง";
+          document.getElementById("text_2").innerHTML = "BorntoDev เป็นแชแนลที่ตอบโจทย์สำหรับโปรแกรมเมอร์มือใหม่เพราะมีเนื้อหาที่คลอบคลุมและทำวิดิโอออกมาสนุกและเข้าใจง่าย";
           break;
         case 'Sports':
           document.getElementById("img_3").innerHTML = "<img src='../img/sport_1.jpg'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/sport_2.jpg'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/sport_3.jpg'>";
+          document.getElementById("text_1").innerHTML = "TU Fleshy Game เป็นกิจกรรมที่ให้นักศึกษาปี1 เข้าร่วมเพื่อความสนุกและได้รู้จักกับเพื่อนใหม่ในรั้วมหาลัยธรรมศาสตร์";
+          document.getElementById("text_2").innerHTML = "TU DOME FirstMeet เป็นกิจกรรมที่ TU DOME จัดขึ้นเพื่อให้น้องใหม่ที่จะเข้ามาในชมรมได้ทำความรู้จักหน้าคล่าตากับเพื่อนๆพี่ๆในชมรม";
           break;
         case 'Competition':
           document.getElementById("img_3").innerHTML = "<img src='../img/Competition_1.jpg'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/Competition_2.jpg'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/Competition_3.jpg'>";
+          document.getElementById("text_1").innerHTML = "Hosting Dynamic web ขึ้น AWS ซึ่งได้เรียนมาจากในCS100 เเล้วนำมาทำเป็นโปรเจคต่อ";
+          document.getElementById("text_2").innerHTML = "ซึ่งทำให้ได้ความรู้ความเข้าใจเกี่ยวกับ AWS เเละการทำงานกันเป็นทีมผ่าน git มากขึ้น";
           break;
         case 'Computer Science':
           document.getElementById("img_3").innerHTML = "<img src='../img/comsci_1.jpg'>";
           document.getElementById("img_4").innerHTML = "<img src='../img/comsci_2.jpg'>";
           document.getElementById("img_5").innerHTML = "<img src='../img/comsci_3.jpg'>";
+          document.getElementById("text_1").innerHTML = "กิจกรรมรับน้องที่พี่ๆในสาขาวิทยาการคอมพิวเตอร์จัดให้ เพื่อให้เข้าในคอนเซ็บคร่าวของการคิดเเบบเป็นระบบในการเขียนโปรเเกรมผ่านการเล่นเกม";
+          document.getElementById("text_2").innerHTML = "เเละกิจกรรมสันธนาการที่ทำให้ได้รู้จักเพื่อนๆพี่ๆในสาขามากขึ้นและยังเป็นกิจกรรมที่สนุกมากๆอีกด้วย";
           break;
       }
 
-      document.getElementById("FnameLname").innerHTML = "<p>" + fullname + "</p>";
+      document.getElementById("FnameLname").innerHTML = "<p>Hi," + fullname + "</p>";
       document.getElementById("studen_id").innerHTML = "<p>" + studentID + "</p>";
       document.getElementById("S-email").innerHTML = "<p>" + email + "</p>";
       document.getElementById("date").innerHTML = "<p>" + startDate + "</p>";
